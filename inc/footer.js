cc15b14739b6158439baf81ea62acf155077b63e
Tim G <tgalvin05@gmail.com>
2013-11-04 15:30:17 -0800

    First Files

---
+++ inc/footer.js
@@ -0,0 +1,8 @@
+document.write('         <div class="row">');
+document.write('           <div class="col-lg-12">');
+document.write('            <ul class="list-unstyled">');
+document.write('               <li class="pull-right"><a href="#top">Back to top</a></li>');
+document.write('             </ul>');
+document.write('             <p>Developed by <a href="">F2eX</a></p>');
+document.write('           </div>');
+document.write('          </div>');
