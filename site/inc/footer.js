a779f520f37daecd7f060a4d2ea4d812361e06a0
timgalvin <tgalvin@uw.edu>
2013-11-21 19:23:49 -0800

    Add privacy policy link

--- site/inc/footer.js
+++ site/inc/footer.js
@@ -4,7 +4,7 @@ document.write('            <ul class="list-unstyled">');
 document.write('               <li class="pull-right"><a href="#top">Back to top</a></li>');
 document.write('             </ul>');
 document.write('             <p>Copyright 2013 <a href="">F2eX</a></p>');
-document.write('             <p>This page is licensed under the <a href="COPYING">GNU GPL V3</a></p>');
+document.write('             <p>This page is licensed under the <a href="COPYING">GNU GPL V3</a> | <a href="/privacy.html">Privacy</a></p>');
 document.write('           </div>');
 document.write('          </div>');
 document.write('<script src="./assets/js/jquery.min.js"></script>');
