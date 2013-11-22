1f79bb0da7202321c3d9d23163f63172510e9302
Tim Galvin <tgalvin@uw.edu>
2013-11-21 18:52:01 -0800

    Added copyright info to footer

--- site/inc/footer.js
+++ site/inc/footer.js
@@ -3,6 +3,7 @@ document.write('           <div class="col-lg-12">');
 document.write('            <ul class="list-unstyled">');
 document.write('               <li class="pull-right"><a href="#top">Back to top</a></li>');
 document.write('             </ul>');
-document.write('             <p>Developed by <a href="">F2eX</a></p>');
+document.write('             <p>Copyright 2013 <a href="">F2eX</a></p>');
+document.write('             <p>This page is licensed under the <a href="COPYING">GNU GPL V3</a></p>');
 document.write('           </div>');
 document.write('          </div>');
